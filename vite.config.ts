5760cb591187e0a290d86e775a2ffcd57f7e74a0
Aasilahamed <saasilahamed123@gmail.com>
2025-11-24 09:30:32 +0530

    🔧 Fix all merge conflicts for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import path from 'path';
-<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -21,27 +20,4 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true,
   }
-=======
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
->>>>>>> 6e2d611bf0e0b8b3e276610de398eb797a5f7161
 });
